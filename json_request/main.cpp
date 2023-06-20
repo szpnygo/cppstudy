bb21e4d50e6dd3887df65992fa68f5da6262676c
neosu <neo@tcodestudio.com>
2023-06-20 17:22:24 +0800

    add error catch and retry

--- json_request/main.cpp
+++ json_request/main.cpp
@@ -1,28 +1,47 @@
 #include <cpr/cpr.h>
-#include <nlohmann/json.hpp>
-#include <iostream>
-#include <future>
-#include <vector>
-#include <atomic>
 
-std::mutex cout_mutex;  // 保护 std::cout 的锁
+#include <atomic>
+#include <chrono>  // for std::chrono
+#include <future>
+#include <iostream>
+#include <nlohmann/json.hpp>
+#include <thread>  // for std::this_thread
+#include <vector>
+
+std::mutex cout_mutex;        // 保护 std::cout 的锁
 std::atomic<int> counter(0);  // 计数器
+const int MAX_RETRIES = 3;
 
 std::string get_and_process(const std::string& url) {
-    std::lock_guard<std::mutex> lock(cout_mutex);  // 保护计数器的锁
-    ++counter;
-    cpr::Response r = cpr::Get(cpr::Url{url});
-    if (r.status_code == 200){
-        nlohmann::json j = nlohmann::json::parse(r.text);
-        std::ostringstream oss;
-        oss << "URL: " << url << '\n';
-        oss << "Username: " << j["login"] << '\n';
-        oss << "ID: " << j["id"] << '\n';
-        return oss.str();
+    for (int i = 0; i < MAX_RETRIES; ++i) {            // 重试循环
+        std::lock_guard<std::mutex> lock(cout_mutex);  // 保护计数器的锁
+        ++counter;
+        cpr::Response r =
+            cpr::Get(cpr::Url{url}, cpr::Timeout{1000});  // 设置超时为1000毫秒
+        if (r.status_code == 200) {
+            try {
+                nlohmann::json j = nlohmann::json::parse(r.text);
+                std::ostringstream oss;
+                oss << "URL: " << url << '\n';
+                oss << "Username: " << j["login"] << '\n';
+                oss << "ID: " << j["id"] << '\n';
+                return oss.str();
+            } catch (nlohmann::json::parse_error& e) {  // 添加错误处理
+                std::cout << "Parse error: " << e.what() << '\n';
+            }
+        }
+        if (r.error || r.status_code != 200) {  // 错误处理
+            // 休眠一段时间后重试
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+            if (r.error) {
+                std::cout << "Error: " << r.error.message << " " << url << '\n';
+            } else {
+                std::cout << "Status: " << r.status_code << " " << url << '\n';
+            }
+        }
     }
 
-     return "Failed to get user info from " + url + ", status code: " + std::to_string(r.status_code) + '\n';
-   
+    return "Failed to get user info from " + url + '\n';
 }
 
 int main() {
@@ -37,12 +56,13 @@ int main() {
     // 创建并运行线程
     for (const auto& user : users) {
         // 使用 std::async 创建异步操作，并将返回的 future 添加到向量
-        futures.emplace_back(std::async(std::launch::async, get_and_process, user));
+        futures.emplace_back(
+            std::async(std::launch::async, get_and_process, user));
     }
 
     // 等待所有异步操作完成，并打印结果
     for (auto& fut : futures) {
-        fut.wait();  // 等待异步操作完成
+        fut.wait();              // 等待异步操作完成
         std::cout << fut.get();  // 获取并打印结果
     }
 
