a747662616ad106c7a5e01e424530495dde9353b
neosu <neo@tcodestudio.com>
2023-06-30 12:36:17 +0800

    override load and save

--- hnsw/src/main.cpp
+++ hnsw/src/main.cpp
@@ -36,7 +36,167 @@ class MyHNSW : public hnswlib::HierarchicalNSW<dist_t> {
     using hnswlib::HierarchicalNSW<dist_t>::HierarchicalNSW;
 
     void newSaveIndex(const std::string& location) {
-        this->saveIndex(location);
+        std::ofstream output(location, std::ios::binary);
+        std::streampos position;
+
+        hnswlib::writeBinaryPOD(output, "aaaaaaa");
+
+        hnswlib::writeBinaryPOD(output, this->offsetLevel0_);
+        hnswlib::writeBinaryPOD(output, this->max_elements_);
+        hnswlib::writeBinaryPOD(output, this->cur_element_count);
+        hnswlib::writeBinaryPOD(output, this->size_data_per_element_);
+        hnswlib::writeBinaryPOD(output, this->label_offset_);
+        hnswlib::writeBinaryPOD(output, this->offsetData_);
+        hnswlib::writeBinaryPOD(output, this->maxlevel_);
+        hnswlib::writeBinaryPOD(output, this->enterpoint_node_);
+        hnswlib::writeBinaryPOD(output, this->maxM_);
+
+        hnswlib::writeBinaryPOD(output, this->maxM0_);
+        hnswlib::writeBinaryPOD(output, this->M_);
+        hnswlib::writeBinaryPOD(output, this->mult_);
+        hnswlib::writeBinaryPOD(output, this->ef_construction_);
+
+        output.write(this->data_level0_memory_,
+                     this->cur_element_count * this->size_data_per_element_);
+
+        for (size_t i = 0; i < this->cur_element_count; i++) {
+            unsigned int linkListSize =
+                this->element_levels_[i] > 0
+                    ? this->size_links_per_element_ * this->element_levels_[i]
+                    : 0;
+            hnswlib::writeBinaryPOD(output, linkListSize);
+            if (linkListSize)
+                output.write(this->linkLists_[i], linkListSize);
+        }
+        output.close();
+    }
+
+    void newLoadIndex(const std::string& location,
+                      hnswlib::SpaceInterface<dist_t>* s,
+                      size_t max_elements_i = 0) {
+        std::ifstream input(location, std::ios::binary);
+
+        if (!input.is_open())
+            throw std::runtime_error("Cannot open file");
+
+        // get file size:
+        input.seekg(0, input.end);
+        std::streampos total_filesize = input.tellg();
+        input.seekg(0, input.beg);
+
+        char buffer[8];
+        hnswlib::readBinaryPOD(input, buffer);
+        std::cout << buffer << std::endl;
+        hnswlib::readBinaryPOD(input, this->offsetLevel0_);
+        hnswlib::readBinaryPOD(input, this->max_elements_);
+        hnswlib::readBinaryPOD(input, this->cur_element_count);
+
+        size_t max_elements = max_elements_i;
+        if (max_elements < this->cur_element_count)
+            max_elements = this->max_elements_;
+        this->max_elements_ = max_elements;
+        hnswlib::readBinaryPOD(input, this->size_data_per_element_);
+        hnswlib::readBinaryPOD(input, this->label_offset_);
+        hnswlib::readBinaryPOD(input, this->offsetData_);
+        hnswlib::readBinaryPOD(input, this->maxlevel_);
+        hnswlib::readBinaryPOD(input, this->enterpoint_node_);
+
+        hnswlib::readBinaryPOD(input, this->maxM_);
+        hnswlib::readBinaryPOD(input, this->maxM0_);
+        hnswlib::readBinaryPOD(input, this->M_);
+        hnswlib::readBinaryPOD(input, this->mult_);
+        hnswlib::readBinaryPOD(input, this->ef_construction_);
+
+        this->data_size_ = s->get_data_size();
+        this->fstdistfunc_ = s->get_dist_func();
+        this->dist_func_param_ = s->get_dist_func_param();
+
+        auto pos = input.tellg();
+
+        /// Optional - check if index is ok:
+        input.seekg(this->cur_element_count * this->size_data_per_element_,
+                    input.cur);
+        for (size_t i = 0; i < this->cur_element_count; i++) {
+            if (input.tellg() < 0 || input.tellg() >= total_filesize) {
+                throw std::runtime_error(
+                    "Index seems to be corrupted or unsupported");
+            }
+
+            unsigned int linkListSize;
+            hnswlib::readBinaryPOD(input, linkListSize);
+            if (linkListSize != 0) {
+                input.seekg(linkListSize, input.cur);
+            }
+        }
+
+        // throw exception if it either corrupted or old index
+        if (input.tellg() != total_filesize)
+            throw std::runtime_error(
+                "Index seems to be corrupted or unsupported");
+
+        input.clear();
+        /// Optional check end
+
+        input.seekg(pos, input.beg);
+
+        this->data_level0_memory_ =
+            (char*)malloc(max_elements * this->size_data_per_element_);
+        if (this->data_level0_memory_ == nullptr)
+            throw std::runtime_error(
+                "Not enough memory: loadIndex failed to allocate level0");
+        input.read(this->data_level0_memory_,
+                   this->cur_element_count * this->size_data_per_element_);
+
+        this->size_links_per_element_ =
+            this->maxM_ * sizeof(hnswlib::tableint) +
+            sizeof(hnswlib::linklistsizeint);
+
+        this->size_links_level0_ = this->maxM0_ * sizeof(hnswlib::tableint) +
+                                   sizeof(hnswlib::linklistsizeint);
+        std::vector<std::mutex>(max_elements).swap(this->link_list_locks_);
+        std::vector<std::mutex>(this->MAX_LABEL_OPERATION_LOCKS)
+            .swap(this->label_op_locks_);
+
+        this->visited_list_pool_ =
+            new hnswlib::VisitedListPool(1, max_elements);
+
+        this->linkLists_ = (char**)malloc(sizeof(void*) * max_elements);
+        if (this->linkLists_ == nullptr)
+            throw std::runtime_error(
+                "Not enough memory: loadIndex failed to allocate linklists");
+        this->element_levels_ = std::vector<int>(max_elements);
+        this->revSize_ = 1.0 / this->mult_;
+        this->ef_ = 10;
+        for (size_t i = 0; i < this->cur_element_count; i++) {
+            this->label_lookup_[this->getExternalLabel(i)] = i;
+            unsigned int linkListSize;
+            hnswlib::readBinaryPOD(input, linkListSize);
+            if (linkListSize == 0) {
+                this->element_levels_[i] = 0;
+                this->linkLists_[i] = nullptr;
+            } else {
+                this->element_levels_[i] =
+                    linkListSize / this->size_links_per_element_;
+                this->linkLists_[i] = (char*)malloc(linkListSize);
+                if (this->linkLists_[i] == nullptr)
+                    throw std::runtime_error(
+                        "Not enough memory: loadIndex failed to allocate "
+                        "linklist");
+                input.read(this->linkLists_[i], linkListSize);
+            }
+        }
+
+        for (size_t i = 0; i < this->cur_element_count; i++) {
+            if (this->isMarkedDeleted(i)) {
+                this->num_deleted_ += 1;
+                if (this->allow_replace_deleted_)
+                    this->deleted_elements.insert(i);
+            }
+        }
+
+        input.close();
+
+        return;
     }
 };
 
@@ -108,7 +268,9 @@ int main() {
                   << items[it->second - 1].key << std::endl;
     }
 
-    // alg_hnsw.newSaveIndex("../data/index.bin");
+    alg_hnsw.newSaveIndex("../data/index.bin");
+
+    alg_hnsw.newLoadIndex("../data/index.bin", &space);
 
     return 0;
 }
