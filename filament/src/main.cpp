223fb546f1d48fa5d9e3da0bb63ee9c443bb030f
neosu <neo@tcodestudio.com>
2023-07-10 16:55:01 +0800

    add filament library

---
+++ filament/src/main.cpp
@@ -0,0 +1,20 @@
+
+#include <filament/Engine.h>
+#include <filament/IndexBuffer.h>
+#include <filament/Renderer.h>
+#include <filament/Scene.h>
+#include <filament/VertexBuffer.h>
+#include <filament/View.h>
+#include <math/vec3.h>
+#include <math/vec4.h>
+#include <ostream>
+
+using namespace filament;
+using namespace filament::math;
+
+int main() {
+    // 创建引擎
+    Engine* engine = Engine::create();
+
+    return 0;
+}
